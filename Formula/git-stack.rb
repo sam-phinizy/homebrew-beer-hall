0472fea6893eecd3110427a72abbfca53bb40f75
GitHub Action <action@github.com>
2025-09-10 02:32:31 +0000

    Update git-stack formula to v0.3.4

--- Formula/git-stack.rb
+++ Formula/git-stack.rb
@@ -1,24 +1,24 @@
 class GitStack < Formula
   desc "Git stack management tool with interactive TUI"
   homepage "https://github.com/sam-phinizy/git-stack"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/sam-phinizy/git-stack/releases/download/v#{version}/git-stack-macos-amd64"
-      sha256 "38e446191a4eb9e9327b575527e6b04bd421e3b4a8982538d49b30483865efa5"
+      sha256 "a4946048414277ef0cfa6474d339dd09b1491506e966c1823ad430513b092918"
     end
     if Hardware::CPU.arm?
       url "https://github.com/sam-phinizy/git-stack/releases/download/v#{version}/git-stack-macos-arm64"
-      sha256 "e5b28b9d6418b33997e63bd7d776d95f8ee845045236546767702df515e2d8a8"
+      sha256 "355d7dadea185c421cddf0f8839767992b219722a467915fc558c6b4765e8f25"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sam-phinizy/git-stack/releases/download/v#{version}/git-stack-linux"
-      sha256 "409e1d035b7181ba5d164e6e153064c73f664d2959405a19bbc50edbe52da0ad"
+      sha256 "13090d4c44fd40009dce25724ef15652fa7c51d8976df72b2c75d946d46a942d"
     end
   end
 
